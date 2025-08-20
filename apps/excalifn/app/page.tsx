777d74f0496ae8ff6d694292fdb74794eacf292b
adi-ty-a <adi.personal.13@gmail.com>
2025-08-20 18:49:02 +0530

    Update page.tsx

--- apps/excalifn/app/page.tsx
+++ apps/excalifn/app/page.tsx
@@ -1,4 +1,4 @@
-import { Mochiy_Pop_One } from "next/font/google";
+import { Mochiy_Pop_One,Outfit } from "next/font/google";
 import Link from "next/link";
 
 const mochiy = Mochiy_Pop_One({
@@ -7,17 +7,29 @@ const mochiy = Mochiy_Pop_One({
   variable: "--font-mochiy",
 });
 
+const outfit = Outfit({
+  subsets: ["latin"],
+   weight: ["300","700"],
+  variable: "--font-outfit", // optional CSS variable
+});
+
 export default function Home(){
     return <div
-    className="bg-linear-to-b  w-screen h-[1701px] from-[#120066] to-[#000000] flex flex-col items-center">
+    className="bg-linear-to-b  w-screen h-[1701px] from-[#120066] to-[#000000] flex flex-col  items-center">
         <div className="w-[1520px] h-[56px] mt-4 flex items-center justify-between px-2">
-            <h1 className={`${mochiy.className} text-xl`}>playboard</h1>
-            <div className="w-[650px] h-[50px] flex items-center justify-around px-5 rounded-4xl bg-linear-to-b  from-[#000000] to-[#281191]">
+            <h1 className={`${mochiy.className} text-xl`}>Playboard</h1>
+            <div className="w-[650px] h-[50px] flex items-center mr-25 justify-around px-5 rounded-4xl  bg-[linear-gradient(0deg,#281191_-117.86%,#000000_130.36%)]            ">
                 <Link href="/dashboard">Home</Link>
                 <Link href="/dashboard">Product</Link>
                 <Link href="/dashboard">About</Link>
             </div>
             <button>SignUp</button>
         </div>
+    <div className="mt-45 flex flex-col items-center">
+        <h1 className={` text-[49px] ${outfit.className} font-bold `}>Sketch, Share, and Build Together</h1>
+        <h2 className={`${outfit.className} text-[20px]`}>Collaborate live on the same canvas with anyone</h2>
+    </div>
     </div>
 }
+
+
