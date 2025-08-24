d3beb17f4e8bf1e7ec972cea5d2bb3a9a7835d41
avi <adi.personal.13@gmail.com>
2025-08-24 20:06:54 +0530

    framer added

--- apps/excalifn/app/page.tsx
+++ apps/excalifn/app/page.tsx
@@ -1,10 +1,13 @@
+"use client"
 import { Mochiy_Pop_One,Outfit } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { motion, scale, stagger } from "motion/react"
 import picture from "../assets/Playboard.svg"
 import { SiInstagram } from "react-icons/si";
 import { SlSocialTwitter } from "react-icons/sl";
 import { TbBrandLinkedin } from "react-icons/tb";
+import { delay } from "motion";
 const mochiy = Mochiy_Pop_One({
   weight: "400", // only one weight available
   subsets: ["latin"],
@@ -18,6 +21,26 @@ const outfit = Outfit({
 });
 
 export default function Home(){
+
+    const parent ={
+        animate:{
+            transition:{
+                staggerChildren:0.8
+            }
+        }
+    }
+
+    const child={
+    visible:{
+        y:0,
+        opacity:100,
+    },
+    initial:{
+        y:50,
+        opacity:0
+    }
+    }
+
     return <>
   <div className="absolute top-[75px] left-0 w-full h-[1px] bg-gray-300 opacity-10"></div>
     <div className="absolute  left-[275px] w-[1px] h-[1701px] bg-gray-300 opacity-10"></div>
@@ -27,7 +50,7 @@ export default function Home(){
         <div>
         <div className="w-[1280px] h-[56px] mt-4 flex items-center justify-between px-2">
             <div className="w-[120px] drop-shadow-md">
-            <h1 className={`${mochiy.className} text-2xl`}>Playboard</h1>
+            <h1 className={`${mochiy.className} text-2xl`} >Playboard</h1>
             </div>
             <div className="w-[650px] h-[50px] p-[2px]  bg-linear-to-b from-[#2300C3] to-[#02000A] rounded-full drop-shadow-lg">
             <div className="w-[650px] h-[50px] flex items-center  justify-around px-5 rounded-full  bg-[linear-gradient(0deg,#281191_-117.86%,#000000_130.36%)]">
@@ -37,30 +60,55 @@ export default function Home(){
             </div>
             </div>
             <div className="w-[120px]">
-            <button className="flex flex-row justify-center items-center p-[10px] gap-[10px] 
+            <motion.button 
+            whileHover={{
+                scale:1.05,
+            }}
+            transition={{
+                delay:0.5
+            }}
+            className="flex flex-row justify-center items-center p-[10px] gap-[10px] 
              w-[100px] h-[39px] 
              bg-gradient-to-b from-[#2E00FF] to-[#13006C] 
-             drop-shadow-md rounded-[15px] 
-             flex-none order-2 grow-0">SignUp</button>
+             drop-shadow-md rounded-[15px] hover:from-[#0073ff] hover:to-[#003473]
+             flex-none order-2 grow-0">SignUp</motion.button>
             </div>
         </div>
-    <div className="mt-45 flex flex-col items-center">
-        <h1 className={` text-[49px] ${outfit.className} font-bold `}>Sketch, Share, and Build Together</h1>
-        <h2 className={`${outfit.className} text-[20px]`}>Collaborate live on the same canvas with anyone</h2>
-    </div>
-        <div className="mt-10 flex flex-col items-center">
+    <motion.div 
+    variants={parent}
+    initial="initial"
+    animate="visible"
+    >
+    <motion.div variants={child} className="mt-45 flex flex-col items-center">
+        <motion.h1 variants={child} className={` text-[49px] ${outfit.className} font-bold relative z-10`}>Sketch, Share, and Build Together</motion.h1>
+        <motion.h2 variants={child} className={`${outfit.className} text-[20px]`}>Collaborate live on the same canvas with anyone</motion.h2>
+    </motion.div>
+        <motion.div variants={child} className="mt-10 flex flex-col items-center">
             <button className={`flex flex-row justify-center items-center p-[10px] gap-[10px] 
-             w-[210px] h-[52px] 
+             w-[210px] h-[52px] relative z-10
              bg-gradient-to-b from-[#D800FF] to-[#8E009B] 
-             drop-shadow-md rounded-[8px]     shadow-[0_0_20px_rgba(216,0,255,0.7)]
+             drop-shadow-md rounded-[12px]     shadow-[0_0_20px_rgba(216,0,255,0.7)]
              flex-none order-2 grow-0 text-xl font-medium text-shadow-white border-b-[1px] border-r border-l border-white ${outfit.className}`}>
             Start Board
             </button>
-        </div>
-        <div className='relative'>
+        </motion.div>
+    </motion.div>
+        <motion.div 
+        initial={{
+            opacity:0,
+            y:50
+        }}
+        animate={{
+            opacity:100,
+            y:0
+        }}
+        transition={{
+            duration:1
+        }}
+        className='relative'>
             <Image  className="relative z-10 mask-b-from-20% mask-b-to-80%" alt="online sketchboard image" src={picture} /> 
             <div className=" absolute left-[425px] inset-0 w-[500px] h-[500px] bg-blue-700 blur-[200px] z-0"></div>
-        </div>
+        </motion.div>
        
         </div>
         <div className="w-full h-[250px] bg-gradient-to-b from-black flex items-center justify-center to-[#120066]">
