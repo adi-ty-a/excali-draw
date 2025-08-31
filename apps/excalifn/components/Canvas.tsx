2731acdace0f3397fd4e11e2612ee1318e747637
Aditya Srivastava <adi.personal.13@gmail.com>
2025-08-31 22:41:06 +0530

    selection highligh

--- apps/excalifn/components/Canvas.tsx
+++ apps/excalifn/components/Canvas.tsx
@@ -18,14 +18,15 @@ export function Canvaspage({roomid,WebSocket}:{
 
     return <div>
         <button className="p-1 m-1 bg-green-500 rounded-md text-white" onClick={()=> {
-                        console.log(tool.current)
-            return tool.current="circle"} }>Circle</button>
+            console.log(tool.current)
+            return tool.current="circle"} 
+        }>Circle</button>
         <button className="p-1 m-1 bg-purple-500 rounded-md text-white"onClick={()=> {
             console.log(tool.current)
             return tool.current="rec"}} >Rectagle</button>
         <button className="p-1 m-1 bg-blue-500 rounded-md text-white" onClick={()=> {
             console.log(tool.current)
-            return tool.current="line"}}>Line</button>
+            return tool.current="select"}}>Line</button>
         <canvas ref={usecanvas} width={1920} height={1080}></canvas>
     </div>
 }
--- apps/excalifn/draw/draw.tsx
+++ apps/excalifn/draw/draw.tsx
@@ -2,12 +2,14 @@ import axios from "axios"
 import { RefObject } from "react";
 
 type shapes ={
+    id:number,
     type:"rect",
     x:number,
     y:number,
     width:number,
     height:number
 } | {
+    id:number,
     type:"circle",
     x:number,
     y:number,
@@ -18,7 +20,7 @@ type shapes ={
 
 export default async function  intindraw(canvas:HTMLCanvasElement,roomId:string,WebSocket:WebSocket,tool:RefObject<string>){
     const existing: shapes[] =  await getExistingshapes(roomId); 
-
+    let selectedShapeId: number | null = null
     if(canvas){
         const ctx = canvas.getContext("2d");
 
@@ -47,25 +49,37 @@ export default async function  intindraw(canvas:HTMLCanvasElement,roomId:string,
                 clicked = true
                 startx = e.clientX;
                 starty= e.clientY;
+
+                if(tool.current == "select"){
+                    selectedShapeId = checkselection(existing,startx,starty,ctx)
+                    clearcanvas(existing,canvas,ctx,selectedShapeId)
+                    console.log(selectedShapeId);
+                }
         })
         canvas.addEventListener("mouseup",(e)=>{
              clicked = false
              const width = e.clientX-startx;
              const height = e.clientY-starty;
-             const radius = width/2;
              let shape:shapes |null = null;
-             if(tool.current == "circle"){
-                 shape = {
-                    type:"circle",
-                    x:startx,
-                    y:starty,
-                    raidus:radius,
-                    startangle:0,
-                    endangle:6.28
-                 } 
-             }
+              if(tool.current == "circle"){
+                const radius =  Math.abs(width / 2); 
+                const centerX = startx ;
+                const centerY = starty ;
+                shape = {
+                    id: Date.now(),
+                    type: "circle",
+                    x: centerX,
+                    y: centerY,
+                    raidus: radius,
+                    startangle: 0,
+                    endangle: 6.28
+                };
+            }
+
+             
              else if(tool.current == "rec"){
                  shape = {
+                    id:Date.now(),
                     type:"rect",
                     x:startx,
                     y:starty,
@@ -105,23 +119,45 @@ export default async function  intindraw(canvas:HTMLCanvasElement,roomId:string,
 }
 }
 
-function clearcanvas(exisitingshapes:shapes[], canvas:HTMLCanvasElement ,ctx:CanvasRenderingContext2D){
+function clearcanvas(exisitingshapes:shapes[], canvas:HTMLCanvasElement ,ctx:CanvasRenderingContext2D,selectedShapeId?:number | null){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle ="rgb(0,0,0)";
     ctx.fillRect(0,0,canvas.width,canvas.height);
+
     exisitingshapes.map((shapes)=>{
     
         if(shapes.type == "rect"){
             ctx.strokeStyle = "rgb(255,255,255)";
+            ctx.setLineDash([])
         ctx.strokeRect(shapes.x,shapes.y,shapes.width,shapes.height);
         }
         else if(shapes.type == "circle"){
+            ctx.setLineDash([])
             ctx.strokeStyle = "rgb(255,255,255)";
             ctx.beginPath();
             ctx.arc(shapes.x,shapes.y,Math.abs(shapes.raidus),0,6.28);
             ctx.stroke();
         }
     })
+    const selectedshape :shapes | undefined = exisitingshapes.find((e)=> e.id == selectedShapeId)
+    if(!selectedshape){
+        return
+    }
+    if(selectedshape.type == "rect" ){
+    ctx.strokeStyle = "rgb(255,158,255)";
+    ctx.setLineDash([20,5]);
+    ctx.strokeRect(
+    selectedshape.x - 15 * Math.sign(selectedshape.width),
+    selectedshape.y - 15 * Math.sign(selectedshape.height),
+    selectedshape.width + 30 * Math.sign(selectedshape.width),
+    selectedshape.height + 30 * Math.sign(selectedshape.height)
+    );
+    }else if(selectedshape.type == "circle" ){
+    ctx.strokeStyle = "rgb(255,158,255)";
+    ctx.setLineDash([20,5]);
+    ctx.strokeRect(selectedshape.x - selectedshape.raidus -15,selectedshape.y - selectedshape.raidus -15,selectedshape.raidus * 2 + 30 ,selectedshape.raidus *2 + 30);
+    }
+    
 }
 
 async function  getExistingshapes(roomId : string){
@@ -136,4 +172,44 @@ async function  getExistingshapes(roomId : string){
         return parsedData.shape;
     })
     return shapes
+}
+
+function checkselection(existing:shapes[],startx:number,starty:number,ctx:CanvasRenderingContext2D){
+    let selectedshape : shapes | null = null;
+    for(let i = existing.length -1 ; i>=0;i--){
+        const isSelected = ispointinshape(existing[i],startx,starty)
+        if(isSelected){
+            selectedshape = existing[i]
+        break;
+        }
+    }
+    if(selectedshape){
+                console.log(selectedshape.type)
+                return selectedshape.id
+    }else{
+        console.log("no shape selected ")
+        return null
+    }
+    return null
+}
+
+function ispointinshape(shape:shapes,startx:number,starty:number,){
+    if(shape.type == 'rect'){
+    const left   = Math.min(shape.x, shape.x + shape.width);
+    const right  = Math.max(shape.x, shape.x + shape.width);
+    const top    = Math.min(shape.y, shape.y + shape.height);
+    const bottom = Math.max(shape.y, shape.y + shape.height);
+        return ( 
+        startx >= left&&
+        startx <= right &&
+        starty >= top && 
+        starty <= bottom
+        )
+    }else if(shape.type == 'circle'){
+    const dx = startx - shape.x;
+    const dy = starty - shape.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance <= shape.raidus
+    }
+    return false;   
 }
